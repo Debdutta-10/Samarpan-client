2ae0233ffdf2ecfd05df87a5172b9cfd28559b60
Debdutta <debduttamukherjee32@gmail.com>
2023-12-15 22:38:18 +0530

    Shloka Page UI Added

--- src/components/Shlokas.js
+++ src/components/Shlokas.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import bg1 from './images/shlokas.jpg';
+import quotes from './Shlokas_data';
+import './styles/shlokas.css'
 
 function Shlokas() {
   return (
-    <div>
-      Shlokas
-    </div>
+    <>
+      <div className="shlok-container">
+        <img src={bg1} alt="" className='shlok-image' />
+        <div className="shlok-text">
+          <p>'O son of Kunti, the nonpermanent appearance of happiness and distress, and their disappearance in due course, are like the appearance and disappearance of winter and summer seasons. They arise from sense perception, O scion of Bharata, and one must learn to tolerate them without being disturbed.'</p>
+          <h2>- Lord Sri Krishna - Bhagavad-gita 2.14</h2>
+        </div>
+      </div>
+      <button className='shloka-button'>Read Another Shlok</button>
+    </>
   )
 }
 
